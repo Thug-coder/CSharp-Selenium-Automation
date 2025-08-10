5837404ebb65f56a22d7d6849d44a0cc99590609
Sahil Shaikh <sahilshaikh3102@gmail.com>
2025-08-10 21:07:32 +0530

    Add project files.

---
+++ UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace Automation_Selenium
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
