d291d5ad176756977e9ee67e056309c727ffbb26
Sahil Shaikh <sahilshaikh3102@gmail.com>
2025-08-10 21:27:04 +0530

    Login using POM

---
+++ Pages/Page_for_Login.cs
@@ -0,0 +1,39 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Automation_Selenium.Pages
+{   
+    public class Page_for_Login
+    {
+        private readonly IWebDriver driver;
+        public Page_for_Login(IWebDriver driver)
+        {
+            this.driver = driver;
+        }
+
+        IWebElement LoginLink => driver.FindElement(By.Id("loginLink"));
+        IWebElement TxtUsername => driver.FindElement(By.Id("UserName"));
+        IWebElement TxtPassword => driver.FindElement(By.Id("Password"));
+        IWebElement BtnLogin => driver.FindElement(By.Id("loginIn"));
+
+        public void ClickLogin()
+        {
+            LoginLink.Click();
+        }
+
+        public void SendUsernamePassword(string Username, string Password)
+        {
+            TxtUsername.SendKeys(Username);
+            TxtPassword.SendKeys(Password);
+        }
+
+        public void ClickLoginBtn()
+        {
+            BtnLogin.Click();
+        }
+    }
+}
--- UnitTest1.cs
+++ UnitTest1.cs
@@ -1,3 +1,8 @@
+using Automation_Selenium.Pages;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System.Net;
+
 namespace Automation_Selenium
 {
     public class Tests
@@ -8,9 +13,20 @@ namespace Automation_Selenium
         }
 
         [Test]
-        public void Test1()
+        public void LoginTest()
         {
-            Assert.Pass();
+            IWebDriver driver = new ChromeDriver();
+
+            driver.Navigate().GoToUrl("http://eaapp.somee.com/");
+
+            driver.Manage().Window.Maximize ();
+
+            Page_for_Login loginPage = new Page_for_Login(driver);
+
+            loginPage.ClickLogin();
+            loginPage.SendUsernamePassword("admin", "password");
+            loginPage.ClickLoginBtn();
+
         }
     }
 }
